92cf0e34bd57e4bf2e1d1b6b8c87eb5c96319e1d
LenyPython <buniadyserv@gmail.com>
2021-02-19 20:53:47 +0100

    random tables for sorting algos

--- sortalg.py
+++ sortalg.py
@@ -1,3 +1,12 @@
+from random import randint
+
+
+
+# test cases
+smallList = [randint(0,10000) for _ in range(100)]
+mediumList = [randint(0,10000) for _ in range(5000)]
+bigList = [randint(0,10000) for _ in range(10000)]
+
 # insert sort
 
 #merge sort
