21f5d0024647b738f146fe33d1ae6c644420d012
LenyPython <buniadyserv@gmail.com>
2021-02-20 15:43:50 +0100

    insert sort algoritm with test functin returning True value if list is sorted

--- sortalg.py
+++ sortalg.py
@@ -12,14 +12,17 @@ testcases = [smallList, mediumList, bigList]
 def test(tests):
 	print('Running tests ...')
 	for i, case in enumerate(tests):
-		print(f'Test no. {i}, result for insertSort:{insertSort(case)}')	
+		sortedCase = insertSort(case)
+		result = all(sortedCase[i] <= sortedCase[i + 1] for i in range(len(sortedCase) - 1))
+		print(f'Test no. {i}, result for insertSort: {result}')	
 
 
 # insert sort
 def insertSort(arr):
 	for i,v in enumerate(arr):
-		if v > arr[i + 1]:
-			arr[i], arr[i + 1] = arr[i + 1], arr[i]
+		if i + 1 < len(arr):
+			if v > arr[i + 1]:
+				arr[i], arr[i + 1] = arr[i + 1], arr[i]
 		while i:
 			if arr[i] < arr[i - 1]:
 				arr[i], arr[i - 1] = arr[i - 1], arr[i]
