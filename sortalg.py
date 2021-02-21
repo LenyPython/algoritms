e6558230114cab49b3646b3ca825def98100cbb5
Piotr Lenartowicz <buniadyserv@gmail.com>
2021-02-21 13:35:34 +0100

    debug of insertsort, test case changed to dict for adding test names

--- sortalg.py
+++ sortalg.py
@@ -6,15 +6,15 @@ from random import randint
 smallList = [randint(0,10000) for _ in range(100)]
 mediumList = [randint(0,10000) for _ in range(5000)]
 bigList = [randint(0,10000) for _ in range(10000)]
-testcases = [smallList, mediumList, bigList]
+testcases = {'small': smallList, 'medium': mediumList, 'big': bigList}
 
 # test function
 def test(tests):
 	print('Running tests ...')
-	for i, case in enumerate(tests):
-		sortedCase = insertSort(case)
+	for key,item in tests.items():
+		sortedCase = insertSort(item)
 		result = all(sortedCase[i] <= sortedCase[i + 1] for i in range(len(sortedCase) - 1))
-		print(f'Test no. {i}, result for insertSort: {result}')	
+		print(f'Test case: {key}, result for insertSort: {result}')	
 
 
 # insert sort
@@ -23,9 +23,8 @@ def insertSort(arr):
 		if i + 1 < len(arr):
 			if v > arr[i + 1]:
 				arr[i], arr[i + 1] = arr[i + 1], arr[i]
-		while i:
-			if arr[i] < arr[i - 1]:
-				arr[i], arr[i - 1] = arr[i - 1], arr[i]
+		while i and arr[i] < arr[i - 1]:
+			arr[i], arr[i - 1] = arr[i - 1], arr[i]
 			i -= 1
 	return arr
 
