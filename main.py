4ecdb41443e4a5037a7b1b0f83ee4887f3e9fa33
LenyPython <buniadyserv@gmail.com>
2021-02-23 21:54:32 +0100

    changing test functionality and main program for main looping the tests

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from tests.test import test, checkIfSorted
-from alg.sortingalgs import *
+
+def main():
+	while True:
+		print('Sorting algoritm tester')
+		algoritm = input('Choose a sorting algoritm/s (for all write all):')
+		test = input('which test would you like to run?\n
+			(tiny, small, medium, big, all)')
+
+		test()
+		continue = input('Would you like to take another test? y/n')
+		if continue != 'y' and continue != 'n':
+			continue = input('Wrong input, continue? y/n')
+		if continue == 'n': break
+
 
 if __name__ == '__main__':
 	test(insertSort, selectSortMin, selectSortMax)
