0ed137998493b7ab5910b1c147677ead90067b3c
LenyPython <buniadyserv@gmail.com>
2021-03-08 17:46:42 +0100

    rat maze tester for mazes

--- backtrackAlgoritms/ratmaze/main.py
+++ backtrackAlgoritms/ratmaze/main.py
@@ -3,26 +3,19 @@
 testmaze = [[1,1,1,0],
 			[1,0,1,0],
 			[1,0,1,0],
-			[1,0,1,1]]
+			[1,0,0,1]]
 
 n = len(testmaze) - 1
 
-def backtrack(maze, i, j, n, step):
-	step += 1
-	print([i, j], step)
-	if i == n and j == n:
-		print('True')
-		return True
+def backtrack(maze, i, j, n):
+	if i == n and j == n: return True
+
 	if i <= n and j <= n:
 		if maze[i][j]:
-			backtrack(maze, i + 1, j, n, step)
-			backtrack(maze, i, j + 1, n, step)
-	return False
+			return True if backtrack(maze, i + 1, j, n) or backtrack(maze, i, j + 1, n) else False
 
 def main():
-	for r in testmaze:
-		print(r)
-	print(backtrack(testmaze, 0, 0, n, 0))
+	print(backtrack(testmaze, 0, 0, n))
 
 
 if __name__ == '__main__':
