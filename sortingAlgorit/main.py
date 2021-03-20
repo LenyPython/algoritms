04e5a2aefa6de569f48ac24a3ed0d71b16cda82f
LenyPython <buniadyserv@gmail.com>
2021-03-20 17:01:39 +0100

    added user input

--- sortingAlgorit/main.py
+++ sortingAlgorit/main.py
@@ -1,23 +1,22 @@
 from tests.test import test, checkIfSorted
-from alg.sortingalgs import quickSort, mergeSort, bubbleSort, insertSort, selectSortMin, selectSortMax
-
-sortingAlgorithms = {
-		'1':quickSort,
-		'2':mergeSort,
-		'3':bubbleSort,
-		'4':insertSort,
-		'5':selectSortMin,
-		'6':selectSortMax
-		}
 
 def userInput():
-		algoritm = input('Choose a sorting algoritm/s: \n \
-						1.quickSort 2.mergeSort 3.bubbleSort\n \
-						4.insertSort 5.selectSortMin 6.selectSortMax\n \
-						ALL for all' )
+		algorithm = input('Choose a sorting algoritm/s: \n \
+				1.quickSort 2.mergeSort 3.bubbleSort\n \
+				4.insertSort 5.selectSortMin 6.selectSortMax\n \
+				ALL for all' )
 		case = input('which test would you like to run?\n \
-			1. tiny 2. small 3. medium 3. big a. all)')
-		repeat = input('How many repeats? ')
+			1. tiny 2. small 3. medium 4. big a. all )')
+		repeat = int(input('How many repeats? '))
+		if algorithm == 'all' or algorithm == 'ALL':
+			algorithm = ['1', '2', '3', '4', '5', '6']
+		else:
+			algorithm = [size for size in algorithm if size.isdigit()]
+
+		if case == 'all':
+			case = ['1', '2', '3', '4']
+		else:
+			case = [size for size in case if size.isdigit()]
 		return {'alg': algorithm, 'size': case, 'times': repeat }
 
 # main I/O function for running specific tests
@@ -29,7 +28,7 @@ def main():
 		
 		# run test function with chosen allgorithms
 		# takes list size, times of repeat and choosen algorithms and *args
-		test(choice['size'], choice['times'], sortingAlgorithms[choice['alg']])
+		test(choice['size'], choice['times'], choice['alg'])
 
 		cont = input('Would you like to take another test? y/n')
 		while cont != 'y' and cont != 'n':
@@ -38,4 +37,8 @@ def main():
 
 
 if __name__ == '__main__':
-	 test(sortingAlgorithms['4'])
+
+	main()
+
+
+
--- sortingAlgorit/tests/test.py
+++ sortingAlgorit/tests/test.py
@@ -1,28 +1,45 @@
 from random import randint
+from alg.sortingalgs import quickSort, mergeSort, bubbleSort, insertSort, selectSortMin, selectSortMax
+
 
 #check if the array is sorted
 def checkIfSorted(arr):
 	return all(arr[i] <= arr[i + 1] for i in range(len(arr) - 1))
 
+def createTest(size):
+	return [randint(0,size) for _ in range(size)]
+
 # test function
-def test(*args):
+def test(size, repeat, *args):
 	#	testcase = input('Choose test size: 0, 1, 2, 3')
 	TESTS = {
-			'tiny': [randint(0,19) for _ in range(10)],
-			'small': [randint(0, 200) for _ in range(100)],
-			'medium': [randint(0,10000) for _ in range(5000)],
-			'big': [randint(0,10000) for _ in range(10000)]
+			'1': createTest(10),
+			'2': createTest(100),
+			'3': createTest(1000),
+			'4': createTest(10000)
 			}
-	for func in args:
-		print(f'Running tests for: {func.__name__}\n')
-		for key, case in TESTS.items():
-			sortedArray = func(case)
-			# sortedArray is a dictionary with sorted arr and time
-			# keys ar result -> sorted array, time -> time elapsed
-			result = checkIfSorted(sortedArray['result'])
-			time = sortedArray['time']
-			print(f'Test case {key} is sorted: {result}, in: {time:.6f} sec')
-		print('...................................\n')
+
+	sortingAlgorithms = {
+			'1':quickSort,
+			'2':mergeSort,
+			'3':bubbleSort,
+			'4':insertSort,
+			'5':selectSortMin,
+			'6':selectSortMax
+			}
+	
+	for key in args:
+		for s in size:
+			for time in range(1, repeat + 1):
+				print(f'Running tests for: {sortingAlgorithms[key].__name__}. Test no. {time}\n')
+				sortedArray = sortingAlgorithms[key](TESTS[s])
+				# sortedArray is a dictionary with sorted arr and time
+				# keys are result -> sorted array, time -> time elapsed
+				result = checkIfSorted(sortedArray['result'])
+				#time spent on testes t
+				t = sortedArray['time']
+				print(f'Test case {size} is sorted: {result}, in: {t:.6f} sec')
+			print('...................................\n')
 		
 
 
