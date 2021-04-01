dcd60d8892430458a16f704c46ce63469a7d760a
LenyPython <buniadyserv@gmail.com>
2021-04-01 17:47:54 +0200

    working pathfind algorithm, just to check if way exists

--- backtrackAlgoritms/ratmaze/main.py
+++ backtrackAlgoritms/ratmaze/main.py
@@ -1,26 +1,29 @@
 from generator.generator import generateMaze
 from pathfinder.findpaths import findPaths
 import numpy as np
+import time
 
 
-def backtrack(maze, n, i = 0, j = 0, goDown = True, goRight = True):
+def backtrack(maze, i = 0, j = 0, came_from=None):
+	time.sleep(0.1)
+	n = len(maze) - 1
 	if i == n and j == n: return True
 	if i <= n and j <= n:
 		if maze[i][j]:
 			print([i, j], maze[i][j])
-			up, down, left, right = False, False, False, False
-			if goDown:
-				down = backtrack(maze, i + 1, j, n)
-			if goRight:
-				right = backtrack(maze, i, j + 1, n)
-			if i > 0 and j > 1:
-				if not maze[i - 1][j - 1]:
-					up = backtrack(maze, i - 1, j, n, False, True)
-			############# debug
-			if i > 1 and j > 0:
-				if not maze[i - 1][j - 1] or not maze[i][j - 1]:
-					left = backtrack(maze, i, j - 1, n, True, False)
-			return True if any([up, down, left, right]) else False
+			if came_from != 'down':
+				if backtrack(maze, i + 1, j, 'top'): return True
+			if came_from != 'right':
+				if backtrack(maze, i, j + 1, 'left'): return True
+			if came_from != 'top':
+				if i > 0 and j > 1:
+					if not maze[i - 1][j - 1]:
+						if backtrack(maze, i - 1, j, 'down'): return True
+			if came_from != 'left':
+				if i > 1 and j > 0:
+					if not maze[i - 1][j - 1] or not maze[i][j - 1]:
+						if backtrack(maze, i, j - 1, 'right'): return True
+			return False
 
 def userInput():
 	n = ''
@@ -35,7 +38,6 @@ def userInput():
 
 def main():
 	n = userInput()
-	arrLen = n - 1
 	#mainMaze = generateMaze(n)
 	mainMaze = np.array(
 				[[1,1,0,1,1,1],
@@ -47,11 +49,11 @@ def main():
 				)
 				
 	print(mainMaze)
-	print(f'Has a solution: {backtrack(mainMaze, arrLen)}')
-#	while not backtrack(mainMaze, arrLen):
+	print(f'Has a solution: {backtrack(mainMaze)}')
+#	while not backtrack(mainMaze):
 #		mainMaze =generateMaze(n)
 #		print(mainMaze)
-#		print(f'Has a solution: {backtrack(mainMaze, arrLen)}')
+#		print(f'Has a solution: {backtrack(mainMaze)}')
 
 
 if __name__ == '__main__':
